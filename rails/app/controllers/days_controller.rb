f7db967e3d547a9c919db8ee7840f6c624ba5012
Joe Masilotti <joe@masilotti.com>
2023-12-06 14:25:36 -0800

    Show entries in order of creation

--- rails/app/controllers/days_controller.rb
+++ rails/app/controllers/days_controller.rb
@@ -6,10 +6,10 @@ class DaysController < ApplicationController
   end
 
   def show
-    @exercise_entries = ExerciseEntry.where(occurred_on: @date)
-    @medication_entries = MedicationEntry.where(occurred_on: @date)
+    @exercise_entries = ExerciseEntry.where(occurred_on: @date).order(:created_at)
+    @medication_entries = MedicationEntry.where(occurred_on: @date).order(:created_at)
     @water = WaterEntry.where(occurred_on: @date).sum(:amount)
-    @food_entries = FoodEntry.where(occurred_on: @date)
+    @food_entries = FoodEntry.where(occurred_on: @date).order(:created_at)
   end
 
   private
