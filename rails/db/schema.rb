9084ed975483af077059452dc1b4d40576175cb4
Joe Masilotti <joe@masilotti.com>
2023-12-05 22:35:40 -0800

    Check in PostgreSQL extension

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_05_220854) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "exercise_entries", force: :cascade do |t|
     t.string "name", null: false
     t.date "occurred_on", null: false
