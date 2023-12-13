0fdb5269fb5390b87b5b1506f28da3c052bffac6
Joe Masilotti <joe@masilotti.com>
2023-12-13 07:09:23 -0800

    Extract private method to a scope

--- rails/app/controllers/days_controller.rb
+++ rails/app/controllers/days_controller.rb
@@ -8,7 +8,7 @@ class DaysController < ApplicationController
   def show
     @exercise_entries = current_user.exercise_entries.where(occurred_on: @date).order(:created_at)
     @medication_entries = current_user.medication_entries.where(occurred_on: @date).order(:created_at)
-    @water = water_amounts.sum.convert_to(current_user.settings.water_unit)
+    @water = current_user.water_entries.where(occurred_on: @date).amounts.sum.convert_to(current_user.settings.water_unit)
     @food_entries = current_user.food_entries.where(occurred_on: @date).order(:created_at)
   end
 
@@ -18,14 +18,5 @@ class DaysController < ApplicationController
     @date = Date.parse(params[:id].to_s)
   rescue Date::Error
     redirect_to day_path(Date.current)
-  end
-
-  def water_amounts
-    amounts = current_user.water_entries.where(occurred_on: @date).map(&:amount)
-    if amounts.any?
-      amounts
-    else
-      [Measured::Volume.new(0, :oz)]
-    end
   end
 end
--- rails/app/models/water_entry.rb
+++ rails/app/models/water_entry.rb
@@ -4,6 +4,7 @@ class WaterEntry < ApplicationRecord
   belongs_to :user
 
   scope :recent, -> { distinct(:amount).order(created_at: :desc).limit(3) }
+  scope :amounts, -> { map(&:amount).presence || [Measured::Volume.new(0, :oz)] }
 
   measured_volume :amount
 
