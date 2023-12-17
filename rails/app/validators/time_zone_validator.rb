392382227195e2beb068862a5096f3bd53b6c679
Joe Masilotti <joe@masilotti.com>
2023-12-16 16:29:18 -0800

    Remove debug call

--- rails/app/validators/time_zone_validator.rb
+++ rails/app/validators/time_zone_validator.rb
@@ -1,7 +1,6 @@
 class TimeZoneValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless ActiveSupport::TimeZone[value].present?
-      binding.pry
       record.errors.add(attribute, (options[:message] || "is not a valid time zone"))
     end
   end
