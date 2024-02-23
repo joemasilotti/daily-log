be1bdcc090b403bf6c46eac951c9ef0c33c5e9bc
Joe Masilotti <joe@masilotti.com>
2024-02-23 06:35:49 -0800

    Standardize

--- rails/config/puma.rb
+++ rails/config/puma.rb
@@ -13,9 +13,9 @@ threads min_threads_count, max_threads_count
 
 # Specifies that the worker count should equal the number of processors in production.
 # if ENV["RAILS_ENV"] == "production"
-  # require "concurrent-ruby"
-  # worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  # workers worker_count if worker_count > 1
+# require "concurrent-ruby"
+# worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+# workers worker_count if worker_count > 1
 # end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
